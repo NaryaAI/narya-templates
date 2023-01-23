a0e33c2b4cce6008f4212e910b6cc6456301716c
pvorona <vorona.pavel.nickolaevich@gmail.com>
2023-01-23 17:54:05 +0300

    Rename `Unbounded Profit` -> `Designed Profit Range Broken`

--- templates/DesignedProfitRangeBrokenTest.sol
+++ templates/DesignedProfitRangeBrokenTest.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "@pwnednomore/contracts/invariants/UnboundedProfitTest.sol";
+import "@pwnednomore/contracts/invariants/DesignedProfitRangeBrokenTest.sol";
 
 // Template documentation: https://www.notion.so/pwned-no-more/Unbounded-Profit-aba3c75268134cf08c9cdae68b31ef64
-contract UnboundedProfitTestImpl is UnboundedProfitTest {
+contract DesignedProfitRangeBrokenTestImpl is DesignedProfitRangeBrokenTest {
 
     function deploy() public override {
         return;
